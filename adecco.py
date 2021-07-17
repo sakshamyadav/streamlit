172b30f8803d86f62ce6bbb2c96ca77fe29a15ed
Saksham Yadav <sakshamyadav11@gmail.com>
2021-07-18 03:52:17 +1000

    Add files via upload

---
+++ adecco.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+import numpy as np 
+import pandas as pd 
+
+hide_streamlit_style = """
+            <style>
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
